7c4d341c5a65f016b732b1f7aeabec5b4705c748
Seunghyun Lee <dedoogong@gmail.com>
2018-05-03 23:55:04 +0000

    cudnn & cv install script

---
+++ cudnn.sh
@@ -0,0 +1,7 @@
+# install cuDNN v6.0
+CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
+tar -xzvf ${CUDNN_TAR_FILE}
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
